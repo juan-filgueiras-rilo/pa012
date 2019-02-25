ab45d40bd62d51e4b11ad7722694faeb98dda4e9
Juan Luis Filgueiras Rilo <juan.filgueiras.rilo@udc.es>
2019-02-25 19:16:20 +0100

    Category

---
+++ backend/src/main/java/es/udc/paproject/backend/model/entities/Category.java
@@ -0,0 +1,23 @@
+package es.udc.paproject.backend.model.entities;
+
+public class Category {
+
+	private long id;
+	private String name;
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}	
+}
